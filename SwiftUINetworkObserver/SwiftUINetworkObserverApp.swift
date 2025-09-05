480796b4a2d15b4bdcbe547f306e971b1daec80a
Belkadi Sidahmed <sidahmed.belkadi0@gmail.com>
2025-09-06 00:34:17 +0100

    Initial Commit

---
+++ SwiftUINetworkObserver/SwiftUINetworkObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINetworkObserverApp.swift
+//  SwiftUINetworkObserver
+//
+//  Created by Sidahmed BELKADI on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINetworkObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
